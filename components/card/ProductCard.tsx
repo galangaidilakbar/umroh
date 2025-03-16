19149a2fdd04f626cd405e35aa94319fc7a7d2fd
Galang Aidil Akbar <galangaidil45@gmail.com>
2025-03-16 17:01:07 +0700

    WIP

--- components/card/ProductCard.tsx
+++ components/card/ProductCard.tsx
@@ -1,22 +1,33 @@
+import Image from "next/image";
+
 export default function ProductCard({
   product,
 }: {
   product: App.Data.ProductData;
 }) {
   return (
-    <div className="card">
-      <div className="card-header">
-        <h2>{product.title}</h2>
+    <div className="rounded-lg bg-white p-4 shadow mb-4">
+      <div className="w-full relative rounded-lg overflow-hidden mb-4">
+        <Image
+          src="placeholder_400_200.svg"
+          alt={product.title}
+          width={400}
+          height={200}
+          className="w-full h-auto"
+        />
       </div>
-      <div className="card-body">
-        <p>Duration: {product.duration}</p>
-        <p>Departure Date: {product.departureDate}</p>
-        <p>Hotels: {product.hotels.join(", ")}</p>
-        <p>Airline: {product.airLine}</p>
-        <p>Departure Airport: {product.departureAirport}</p>
-        <p>Remaining Seats: {product.remainingSeats}</p>
-        <p>Total Seats: {product.totalSeats}</p>
-        <p>Price: ${product.price}</p>
+
+      <div className="space-y-3">
+        <div className="flex justify-between items-start">
+          <div>
+            <h3 className="font-bold text-lg text-gray-900">{product.title}</h3>
+            <p className="text-gray-700">{product.duration}</p>
+          </div>
+          <div className="text-right">
+            <p className="text-sm text-gray-700">Berangkat</p>
+            <p className="font-medium">{product.departureDate}</p>
+          </div>
+        </div>
       </div>
     </div>
   );

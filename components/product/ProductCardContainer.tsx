f9570ea4f62c8f6e225d5adaf0561314fbca9ad0
Galang Aidil Akbar <galangaidil45@gmail.com>
2025-03-17 07:06:06 +0700

    WIP

--- components/product/ProductCardContainer.tsx
+++ components/product/ProductCardContainer.tsx
@@ -1,4 +1,5 @@
 import ProductCard from "./ProductCard";
+import { FaTriangleExclamation } from "react-icons/fa6";
 
 // Helper function to extract month name from date string
 const getMonthName = (dateString: string) => {
@@ -33,6 +34,21 @@ export default function ProductCardContainer({
         return true;
     }
   });
+
+  if (filteredProducts.length === 0) {
+    return (
+      <div className="flex flex-col items-center justify-center p-12 text-center">
+        <FaTriangleExclamation className="mb-4 h-24 w-24 text-neutral-100 dark:text-neutral-200" />
+        <h3 className="mb-2 text-xl font-semibold text-white">
+          No Products Found
+        </h3>
+        <p className="mx-auto max-w-md text-neutral-100 dark:text-gray-200">
+          We could not find any packages matching your filters. Try adjusting
+          your search criteria or clearing the filters.
+        </p>
+      </div>
+    );
+  }
 
   return (
     <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2 lg:grid-cols-3">
